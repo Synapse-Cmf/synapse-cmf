43e457df55d5c6b725a37da2ae673d29237e85b0
Raphael Trzpit <rtrzpit@gmail.com>
2016-12-16 15:49:03 +0100

    Add twig function for decoration

--- src/Synapse/Cmf/Framework/Engine/Bridge/Twig/Extension.php
+++ src/Synapse/Cmf/Framework/Engine/Bridge/Twig/Extension.php
@@ -3,10 +3,12 @@
 namespace Synapse\Cmf\Framework\Engine\Bridge\Twig;
 
 use Synapse\Cmf\Framework\Engine\Renderer\Zone\ZoneRenderer;
+use Synapse\Cmf\Framework\Engine\Engine as Synapse;
 
 /**
  * Extension class which provides Synapse specific tags :
  *   - {{ synapse_zone('name') }}.
+ *   - {{ synapse_decorate$($decorable, ['param' => 12], 'templateTypeName') }}.
  */
 class Extension extends \Twig_Extension
 {
@@ -16,13 +18,19 @@ class Extension extends \Twig_Extension
     protected $zoneRenderer;
 
     /**
+     * @var Synapse
+     */
+    protected $synapse;
+
+    /**
      * Construct.
      *
      * @param ZoneRenderer $zoneRenderer
      */
-    public function __construct(ZoneRenderer $zoneRenderer)
+    public function __construct(ZoneRenderer $zoneRenderer, Synapse $synapse)
     {
         $this->zoneRenderer = $zoneRenderer;
+        $this->synapse = $synapse;
     }
 
     /**
@@ -40,6 +48,7 @@ class Extension extends \Twig_Extension
     {
         return array(
             new \Twig_SimpleFunction('synapse_zone', array($this, 'renderZone'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('synapse_decorate', array($this, 'decorate'), array('is_safe' => array('html'))),
         );
     }
 
@@ -54,5 +63,21 @@ class Extension extends \Twig_Extension
     public function renderZone($name, array $parameters = array())
     {
         return $this->zoneRenderer->render($name, $parameters);
+    }
+
+    /**
+     * Decorates.
+     *
+     * @param ContentInterface|ComponentInterface $decorable
+     * @param array                               $parameters
+     * @param string                              $templateTypeName
+     *
+     * @return string
+     */
+    public function decorate($decorable, $parameters = array(), $templateTypeName = null)
+    {
+        return $this->synapse
+            ->createDecorator($decorable, $templateTypeName)
+            ->decorate($parameters);
     }
 }
