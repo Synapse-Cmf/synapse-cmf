8257145a8ec088fa8bab50263dcb0778e904dab7
Quentin Cerny <quentin.cerny@gmail.com>
2017-01-26 10:23:09 +0100

    Fix missing namespace into template command

--- src/Synapse/Cmf/Framework/Theme/Template/Domain/Command/AbstractCommand.php
+++ src/Synapse/Cmf/Framework/Theme/Template/Domain/Command/AbstractCommand.php
@@ -5,9 +5,10 @@ namespace Synapse\Cmf\Framework\Theme\Template\Domain\Command;
 use Majora\Framework\Domain\Action\ActionInterface;
 use Majora\Framework\Domain\Action\Dal\DalActionTrait;
 use Synapse\Cmf\Framework\Theme\Template\Entity\Template;
+use Synapse\Cmf\Framework\Theme\Template\Event\Event as TemplateEvent;
 use Synapse\Cmf\Framework\Theme\Template\Model\TemplateInterface;
-use Synapse\Cmf\Framework\Theme\Zone\Entity\ZoneCollection;
 use Synapse\Cmf\Framework\Theme\Zone\Domain\DomainInterface as ZoneDomain;
+use Synapse\Cmf\Framework\Theme\Zone\Entity\ZoneCollection;
 
 /**
  * Base class for Template commands.
